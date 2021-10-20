cb006963b99abff888fc882678d04c0cb87f1d63
ItzHungooTheCoder <89455850+ItzHungooTheCoder@users.noreply.github.com>
2021-10-20 09:14:07 +0700

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "\nWelcome to Jet - The basic program coded in C++!\n";
+	std::cout << "\nYou can check the source code in\n";
+	std::cout << "\nhttps://github.com/ItzHungooTheCoder/JETPROJECTCPP"
+	system("pause");
+	return 0;
+}
